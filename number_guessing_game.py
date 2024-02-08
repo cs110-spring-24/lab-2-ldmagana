ca5cf6343f293845c175e89d0f9dec12252d6d15
ldmagana <ldanielamagana@gmail.com>
2024-02-07 16:18:48 -0800

    Update number_guessing_game.py

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,12 +1,20 @@
 import random
 num = random.randint(1, 100)
 
-user = input("Enter your guess: ")
-user = int(user)
+tries = 0 
+user = 0 
 
-if user > num:
-    print("Too high, it was", num)
-elif user < num:
-    print("Too low, it was", num)
-else:
-    print("You got it!")
+while user != num:
+    user = input("Enter your guess: ")
+    user = int(user)
+
+    if user > num:
+        print("Too high")
+        tries = tries + 1
+    elif user < num:
+        print("Too low")
+        tries = tries + 1
+    else:
+        print("You got it! it only took you", tries, "tries!")
+        
+
